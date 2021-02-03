817636c69e54549fec3851763026a4ab092441c4
Shun <shun12baseball@gmail.com>
2021-02-03 19:03:31 +0900

    feat: create get current date in index.ts

--- react-scheduler/src/utils/index.ts
+++ react-scheduler/src/utils/index.ts
@@ -2,8 +2,8 @@
  * get current date
  *
  */
-export function get_current_date() {
+export function get_current_date(): string {
   const date = new Date();
-  return date;
+  return `${date.getFullYear()}-${date.getMonth()}-${date.getDate()}`;
 }
 
