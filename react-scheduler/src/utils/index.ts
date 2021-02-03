59553d52ae65911bae3f4fa4c7b7200d6fee0caf
Shun <shun12baseball@gmail.com>
2021-02-03 19:20:13 +0900

    fix

--- react-scheduler/src/utils/index.ts
+++ react-scheduler/src/utils/index.ts
@@ -4,6 +4,6 @@
  */
 export function get_current_date(): string {
   const date = new Date();
-  return `${date.getFullYear()}-${date.getMonth()}-${date.getDate()}`;
+  return `${date.getFullYear()}-${date.getMonth() + 1}-${date.getDate()}`;
 }
 
