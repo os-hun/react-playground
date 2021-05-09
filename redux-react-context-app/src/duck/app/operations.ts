8e0caab6510b4183f40d77d0472f2b13c0e07abe
Shun <shun12baseball@gmail.com>
2021-05-09 12:49:37 +0900

    feat: READMEを修正

--- redux-react-context-app/src/duck/app/operations.ts
+++ redux-react-context-app/src/duck/app/operations.ts
@@ -7,7 +7,7 @@ import { client } from '../../client';
 
 export function fetch_login_user() {
   return async (dispatch: Dispatch) => {
-    const res = await client.v1.u._screenName('user_1').get();
+    const res = await client.v1.u.me.get();
     console.log(res);
     dispatch(fetch_login_user_action(res.body as any));
   }
