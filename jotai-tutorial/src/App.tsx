c7f6751793fc526684c36a870828bffedd374395
Shun <shun12baseball@gmail.com>
2022-08-27 11:24:55 +0900

    feat: jotaiでカウントを実装

--- jotai-tutorial/src/App.tsx
+++ jotai-tutorial/src/App.tsx
@@ -1,10 +1,15 @@
+import { useAtom } from 'jotai'
+import { countAtom } from './store/count'
 import './App.css'
 
 function App() {
+  const [count, setCount] = useAtom(countAtom)
+
   return (
     <div className="App">
       <header className="App-header">
-        <p>count</p>
+        <p>{count}</p>
+        <button onClick={() => setCount(count + 1)}>increment</button>
       </header>
     </div>
   )
