f0784fda633c67d22eff49ad194f257e2e3e88ad
Shun <shun12baseball@gmail.com>
2022-08-27 11:16:08 +0900

    feat: jotaiを導入. redux関連のlibraryを削除

--- jotai-tutorial/src/App.tsx
+++ jotai-tutorial/src/App.tsx
@@ -1,53 +1,13 @@
-import { useState } from 'react';
-import { useAppDispatch, useAppSelector } from './app/hooks';
-import {
-  increment,
-  decrement,
-  incrementByAmount,
-  selectCount,
-} from './reducers/count';
-import { useCountContext } from './contexts';
-import './App.css';
+import './App.css'
 
 function App() {
-  const count = useAppSelector(selectCount);
-  const dispatch = useAppDispatch();
-  const [incrementAmount, setIncrementAmount] = useState(2);
-  const onSubmit = () => dispatch(incrementByAmount(incrementAmount));
-  const { contextCount, incrementCount, decrementCount } = useCountContext();
-
   return (
     <div className="App">
       <header className="App-header">
-        <p>{count}</p>
-
-        <div>
-          <button onClick={() => dispatch(increment())}>+</button>
-          <button onClick={() => dispatch(decrement())}>-</button>
-        </div>
-
-        <div>
-          <input
-            type="number"
-            value={incrementAmount}
-            onChange={(e) => setIncrementAmount(Number(e.target.value))}
-          />
-          <button onClick={onSubmit}>
-            Add Amount
-          </button>
-        </div>
-
-        <div>
-          <p>{contextCount}</p>
-        </div>
-
-        <div>
-          <button onClick={incrementCount}>+</button>
-          <button onClick={decrementCount}>-</button>
-        </div>
+        <p>count</p>
       </header>
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
--- jotai-tutorial/src/index.tsx
+++ jotai-tutorial/src/index.tsx
@@ -1,24 +1,15 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
-import { store } from './app/store';
-import { Provider } from 'react-redux';
-import { CounterProvider } from './contexts';
+import App from './App'
 import reportWebVitals from './reportWebVitals';
-
-const INITIAL_COUNT = 2;
 
 ReactDOM.render(
   <React.StrictMode>
-    <Provider store={store}>
-      <CounterProvider initialCount={INITIAL_COUNT}>
-        <App />
-      </CounterProvider>
-    </Provider>
+    <App />
   </React.StrictMode>,
   document.getElementById('root')
-);
+)
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
